e432e348ef1eb139a093b68eb35dc4f97654f734
Ammad <mammadkhalil421@gmail.com>
2026-06-29 15:36:08 +0500

    Update index1.js

--- index1.js
+++ index1.js
@@ -1,56 +1,56 @@
-// chap11-13
-// var ch = prompt("Enter a character");
-// var code = ch.charCodeAt(0);
-// if (code >= 48 && code <= 57) {
-//     alert("It is a Number");}
-// else if (code >= 65 && code <= 90) {
-//     alert("It is an Uppercase Letter");}
-// else if (code >= 97 && code <= 122) {
-//     alert("It is a Lowercase Letter");}
-// q.
-// var num1 = +prompt("Enter first number");
-// var num2 = +prompt("Enter second number");
-// if (num1 > num2) {
-//     alert(num1 + " is larger");}
-// else if (num2 > num1) {
-//     alert(num2 + " is larger");}
-// else {
-//     alert("Both numbers are equal");}
+chap11-13
+var ch = prompt("Enter a character");
+var code = ch.charCodeAt(0);
+if (code >= 48 && code <= 57) {
+    alert("It is a Number");}
+else if (code >= 65 && code <= 90) {
+    alert("It is an Uppercase Letter");}
+else if (code >= 97 && code <= 122) {
+    alert("It is a Lowercase Letter");}
+q.
+var num1 = +prompt("Enter first number");
+var num2 = +prompt("Enter second number");
+if (num1 > num2) {
+    alert(num1 + " is larger");}
+else if (num2 > num1) {
+    alert(num2 + " is larger");}
+else {
+    alert("Both numbers are equal");}
 
-// var num = +prompt("Enter a number");
-// if (num > 0) {
-//     alert("Positive Number");
-// }
-// else if (num < 0) {
-//     alert("Negative Number");
-// }
-// else {
-//     alert("Zero");
-// }
+var num = +prompt("Enter a number");
+if (num > 0) {
+    alert("Positive Number");
+}
+else if (num < 0) {
+    alert("Negative Number");
+}
+else {
+    alert("Zero");
+}
 
-// var correctPassword = "admin123";
-// var userPassword = prompt("Enter Password");
-// if (userPassword === "") {
-//     alert("Please enter your password");
-// }
-// else if (userPassword === correctPassword) {
-//     alert("Correct! The password you entered matches the original password");
-// }
-// else {
-//     alert("Incorrect password");
-// }
+var correctPassword = "admin123";
+var userPassword = prompt("Enter Password");
+if (userPassword === "") {
+    alert("Please enter your password");
+}
+else if (userPassword === correctPassword) {
+    alert("Correct! The password you entered matches the original password");
+}
+else {
+    alert("Incorrect password");
+}
 
-// last q
-// var time = +prompt("Enter time in 24-hour format");
-// if (time >= 0 && time < 1200) {
-//     alert("Good Morning");
-// }
-// else if (time >= 1200 && time < 1700) {
-//     alert("Good Afternoon");
-// }
-// else if (time >= 1700 && time < 2100) {
-//     alert("Good Evening");
-// }
-// else if (time >= 2100 && time <= 2359) {
-//     alert("Good Night");
-// }
+last q
+var time = +prompt("Enter time in 24-hour format");
+if (time >= 0 && time < 1200) {
+    alert("Good Morning");
+}
+else if (time >= 1200 && time < 1700) {
+    alert("Good Afternoon");
+}
+else if (time >= 1700 && time < 2100) {
+    alert("Good Evening");
+}
+else if (time >= 2100 && time <= 2359) {
+    alert("Good Night");
+}
